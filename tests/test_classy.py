ad975f6ae9e222f47db38fb9f54dbf35b69a3548
tridelt <Friedrich787>
2021-09-29 10:43:54 +0200

    got it working with docker sdk

--- tests/test_classy.py
+++ tests/test_classy.py
@@ -1,18 +1,42 @@
 import docker
 import ariel.classy as c
 import os
+import pprint
+import json
 
 # def test_two():
 #     assert c.multiplier(4) == 16
 
 def test_hannah():
-    assert os.system('docker run --rm hannah pytest') == 0
+    # assert os.system('docker run --rm hannah pytest') == 0
     # assert os.system('docker run --rm hannah pytest')
-    # client = docker.from_env()
-    # client.containers.run("tridelt/hannah",command='pytest',remove=True,)
+    client = docker.from_env()
+    try:
+        container = client.containers.run("hannah",command='pytest',stderr=True,detach=True)
+        container.wait()
+    except:
+        pass
 
-    # try:
-    #     client.containers.run("hannah",command='pytest',remove=True,)
-    # except:
-    #     pass
-    # assert True
+    out = container.logs(stdout=True, stderr=False)
+    err = container.logs(stdout=False, stderr=True)
+
+    print(out)
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print(out.decode('ascii'))
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print(err)
+    # print(json.loads(str(err)))
+
+
+    # pprint.pprint(err)
+
+    assert False
