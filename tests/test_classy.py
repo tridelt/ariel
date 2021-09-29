73ef785ab2bfd361b16dd44cb77a0dc9f1aa01c9
tridelt <Friedrich787>
2021-09-29 09:33:27 +0200

    backup

--- tests/test_classy.py
+++ tests/test_classy.py
@@ -1,4 +1,15 @@
+import docker
 import ariel.classy as c
 
-def test_two():
-    assert c.multiplier(4) == 16
+# def test_two():
+#     assert c.multiplier(4) == 16
+
+def test_hannah():
+    client = docker.from_env()
+    client.containers.run("hannah",command='pytest',remove=True,)
+
+    # try:
+    #     client.containers.run("hannah",command='pytest',remove=True,)
+    # except:
+    #     pass
+    assert True
