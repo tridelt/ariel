cdda7fd99337c75a6d555e6f7f9614eb7d74fddb
tridelt <Friedrich787>
2021-09-29 10:07:22 +0200

    got it to work with right output

--- tests/test_classy.py
+++ tests/test_classy.py
@@ -1,15 +1,18 @@
 import docker
 import ariel.classy as c
+import os
 
 # def test_two():
 #     assert c.multiplier(4) == 16
 
 def test_hannah():
-    client = docker.from_env()
-    client.containers.run("tridelt/hannah",command='pytest',remove=True,)
+    assert os.system('docker run --rm hannah pytest') == 0
+    # assert os.system('docker run --rm hannah pytest')
+    # client = docker.from_env()
+    # client.containers.run("tridelt/hannah",command='pytest',remove=True,)
 
     # try:
     #     client.containers.run("hannah",command='pytest',remove=True,)
     # except:
     #     pass
-    assert True
+    # assert True
