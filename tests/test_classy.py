d75396419aa670e41ba73f8dca7df5228b96eff6
tridelt <Friedrich787>
2021-09-29 11:11:03 +0200

    got a nicely working version

--- tests/test_classy.py
+++ tests/test_classy.py
@@ -3,6 +3,7 @@ import ariel.classy as c
 import os
 import pprint
 import json
+import time
 
 # def test_two():
 #     assert c.multiplier(4) == 16
@@ -11,32 +12,12 @@ def test_hannah():
     # assert os.system('docker run --rm hannah pytest') == 0
     # assert os.system('docker run --rm hannah pytest')
     client = docker.from_env()
-    try:
-        container = client.containers.run("hannah",command='pytest',stderr=True,detach=True)
-        container.wait()
-    except:
-        pass
+    
+    container = client.containers.run("hannah",command='pytest',detach=True)
+    status_code = container.wait()['StatusCode']
+    out = container.logs(stdout=True, stderr=True).decode('ascii')
+    container.remove()
 
-    out = container.logs(stdout=True, stderr=False)
-    err = container.logs(stdout=False, stderr=True)
-
-    print(out)
-    print('')
-    print('')
-    print('')
-    print('')
-    print('')
-    print('')
-    print(out.decode('ascii'))
-    print('')
-    print('')
-    print('')
-    print('')
-    print('')
-    print(err)
-    # print(json.loads(str(err)))
-
-
-    # pprint.pprint(err)
-
-    assert False
+    if status_code != 0:
+        print(status_code)
+        assert False, out
